f169ec68e845ccb7eb52bb4a5a257ae38e7c86e3
Harsha Surampudi <36491599+HarshaSurampudi@users.noreply.github.com>
2023-02-22 06:15:35 +0000

    Update gen_tax_bot.py

--- gen_tax_bot.py
+++ gen_tax_bot.py
@@ -44,24 +44,36 @@ def generate_answer(query):
     # tokenize the query to get input_ids
     inputs = tokenizer([query], max_length=1024, return_tensors="pt")
     # use generator to predict output ids
-    ids = generator.generate(inputs["input_ids"], num_beams=2, min_length=20, max_length=40)
+    ids = generator.generate(input_ids=inputs["input_ids"].to(device),
+                                           attention_mask=inputs["attention_mask"].to(device),
+                                           min_length=26,
+                                           max_length=256,
+                                           do_sample=False, 
+                                           early_stopping=True,
+                                           num_beams=8,
+                                           temperature=1.0,
+                                           top_k=None,
+                                           top_p=None,
+                                           eos_token_id=tokenizer.eos_token_id,
+                                           no_repeat_ngram_size=3,
+                                           num_return_sequences=1)
     # use tokenizer to decode the output ids
     answer = tokenizer.batch_decode(ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
     return answer
-
-# query = "when was the first electric power system built?"
-# result = query_pinecone(query, top_k=1)
-# query = format_query(query, result["matches"])
-# print(generate_answer(query))
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}. I am a taxbot. Ask me a question about taxes and I will try to answer it. (e.g. What is Income Tax). I am still learning so please be patient.')
 
 async def message(update: Update, context):
     query = update.message.text
-    result = query_pinecone(query, top_k=2)
-    query = format_query(query, result["matches"])
-    print(str(result["matches"]))
+    result = query_pinecone(query, top_k=3)
+    matches = result["matches"]
+    #filter matches with score less than 0.5
+    matches = [m for m in matches if m["score"] >= 0.5]
+    if len(matches) == 0:
+        await update.message.reply_text("Sorry, I don't know the answer to that question. Please try again.")
+        return
+    query = format_query(query, matches)
     await update.message.reply_text(generate_answer(query))
 
 
